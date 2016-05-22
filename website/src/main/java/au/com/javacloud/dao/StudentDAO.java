59566f5f7eea8a2b8215e7e8b9c3d50de2de7acd
Development Team <dev@digitalspider.com.au>
2016-05-22 19:40:26 +1000

    Updated code

--- website/src/main/java/au/com/javacloud/dao/StudentDAO.java
+++ website/src/main/java/au/com/javacloud/dao/StudentDAO.java
@@ -3,5 +3,14 @@ package au.com.javacloud.dao;
 /**
  * Created by david on 22/05/16.
  */
-public class StudentDAO {
+import java.util.List;
+
+import au.com.javacloud.model.Student;
+
+public interface StudentDAO {
+    public void addStudent( Student student );
+    public void deleteStudent( int studentId );
+    public void updateStudent( Student student );
+    public List<Student> getAllStudents();
+    public Student getStudentById( int studentId );
 }
--- website/src/main/java/au/com/javacloud/model/Student.java
+++ website/src/main/java/au/com/javacloud/model/Student.java
@@ -3,5 +3,61 @@ package au.com.javacloud.model;
 /**
  * Created by david on 22/05/16.
  */
+
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String course;
+    private int year;
+
+    @Override
+    public String toString() {
+        return "Student[" + id + "]"+
+                " firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", course='" + course + '\'' +
+                ", year=" + year +
+                ']';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
