7a02cb0493e867d6ccf0fed4229d525684fc17e6
Digital Spider <dev@digitalspider.com.au>
2016-06-04 11:12:41 +1000

    Restructure

--- website/database.sh
+++ website/database.sh
@@ -3,4 +3,4 @@
 DBDIR=/opt/tomcat/tomcat-stage/webapps
 
 sudo rm $DBDIR/database.db
-sudo sqlite3 $DBDIR/database.db -init src/main/resources/students.sql
+sudo sqlite3 $DBDIR/database.db -init src/main/resources/database.sql
