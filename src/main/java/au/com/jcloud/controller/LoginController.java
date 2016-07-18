ea243bc5c1fed90f53fc9842049ef5dc69086b96
Digital Spider <dev@digitalspider.com.au>
2016-07-18 20:27:33 +1000

    Login

---
+++ src/main/java/au/com/jcloud/controller/LoginController.java
@@ -0,0 +1,13 @@
+package au.com.jcloud.controller;
+
+import au.com.jcloud.jcframe.annotation.BeanClass;
+import au.com.jcloud.jcframe.controller.BaseLoginController;
+import au.com.jcloud.model.Login;
+
+/**
+ * Created by david on 18/07/16.
+ */
+@BeanClass(Login.class)
+public class LoginController extends BaseLoginController {
+
+}
---
+++ src/main/java/au/com/jcloud/model/Login.java
@@ -0,0 +1,13 @@
+package au.com.jcloud.model;
+
+import au.com.jcloud.jcframe.annotation.HiddenBean;
+import au.com.jcloud.jcframe.annotation.TableName;
+import au.com.jcloud.jcframe.model.BaseLogin;
+
+/**
+ * Created by david on 18/07/16.
+ */
+@HiddenBean
+@TableName("User")
+public class Login extends BaseLogin {
+}
