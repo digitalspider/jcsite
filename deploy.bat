7d7b2effd2f417e3ed9da99c99f83bc722538b94
David Vittor <davidv@booktopia.com.au>
2016-08-16 16:15:49 +1000

    Maven in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,13 @@
 @echo off
 
-REM mvn -DskipTests package
+CALL mvn -DskipTests package
 
 set TOMCATDIR=\apps\tomcat\apache-tomcat-8.0.35
 
 del \apps\tomcat\apache-tomcat-8.0.35\webapps\website.war
 PING 1.1.1.1 -n 2 -w 60000 >NUL
 copy /Y target\website.war %TOMCATDIR%\webapps\website.war
+
+dir %TOMCATDIR%\webapps\
 
 REM tail -100f %TOMCATDIR%\logs\catalina.out 
