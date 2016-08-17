f71bb51b65a1c575075ce2bb8e3d37a5fa92d48a
David Vittor <davidv@booktopia.com.au>
2016-08-17 13:22:38 +1000

    Updated windows script

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,7 @@ CALL mvn -DskipTests package
 set TOMCATDIR=\apps\tomcat\apache-tomcat-8.0.35
 
 del \apps\tomcat\apache-tomcat-8.0.35\webapps\website.war
+rmdir /S/Q \apps\tomcat\apache-tomcat-8.0.35\webapps\website
 PING 1.1.1.1 -n 2 -w 60000 >NUL
 copy /Y target\website.war %TOMCATDIR%\webapps\website.war
 
