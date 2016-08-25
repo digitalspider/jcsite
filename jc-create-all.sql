02c4f00896faa2a467f18c6361de03d010b5a584
Digital Spider <dev@digitalspider.com.au>
2016-08-25 23:16:28 +1000

    config

--- jc-create-all.sql
+++ jc-create-all.sql
@@ -1,20 +1,20 @@
 create table server (
   id                            bigint auto_increment not null,
-  name                          varchar(255),
+  name                          varchar(63),
   cdate                         date,
   mdate                         date,
-  status                        varchar(255),
-  ip                            varchar(255),
-  alias                         varchar(255),
-  os                            varchar(255),
-  architecture                  varchar(255),
-  osversion                     varchar(255),
-  mem_max                       varchar(255),
-  mem_peak                      varchar(255),
-  memcurrent                    varchar(255),
-  hdd_max                       varchar(255),
-  hdd_peak                      varchar(255),
-  hdd_current                   varchar(255),
+  status                        varchar(16),
+  ip                            varchar(16),
+  alias                         varchar(16),
+  os                            varchar(16),
+  architecture                  varchar(16),
+  osversion                     varchar(16),
+  mem_max                       int,
+  mem_peak                      int,
+  memcurrent                    int,
+  hdd_max                       int,
+  hdd_peak                      int,
+  hdd_current                   int,
   sshkey                        varchar(255),
   parent_server_id              integer,
   constraint pk_server primary key (id)
@@ -22,30 +22,29 @@ create table server (
 
 create table user (
   id                            bigint auto_increment not null,
-  name                          varchar(255),
+  name                          varchar(63),
   cdate                         date,
   mdate                         date,
-  status                        varchar(255),
-  email                         varchar(255),
-  password                      varchar(255),
-  first_name                    varchar(255),
-  last_name                     varchar(255),
+  status                        varchar(16),
+  email                         varchar(63),
+  password                      varchar(63),
+  first_name                    varchar(63),
+  last_name                     varchar(63),
   constraint pk_user primary key (id)
 );
 
 create table role (
-  id                            bigint auto_increment not null,
-  name                          varchar(255),
-  constraint pk_role primary key (id)
+  role                          varchar(16),
+  constraint pk_role primary key (role)
 );
 
 create table userrole (
-  user                          varchar(255),
-  role                          varchar(255),
-  constraint pk_userrole primary key (user,role)
+  name                          varchar(63),
+  role                          varchar(16),
+  constraint pk_userrole primary key (name,role)
 );
 
-insert into role (name) VALUES ('admin');
-insert into role (name) VALUES ('manager');
-insert into role (name) VALUES ('user');
+insert into role (role) VALUES ('admin');
+insert into role (role) VALUES ('manager');
+insert into role (role) VALUES ('user');
 
--- jc-drop-all.sql
+++ jc-drop-all.sql
@@ -1,4 +1,4 @@
 drop table if exists server;
-
 drop table if exists user;
-
+drop table if exists role;
+drop table if exists userrole; table if exists user;
