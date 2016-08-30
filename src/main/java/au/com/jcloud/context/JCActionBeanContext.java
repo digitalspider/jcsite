1bca3a63d20d8011d13df259fbb38a99223471e4
David Vittor <davidv@booktopia.com.au>
2016-08-30 16:02:31 +1000

    Removed JCFrame

--- src/main/java/au/com/jcloud/context/JCActionBeanContext.java
+++ src/main/java/au/com/jcloud/context/JCActionBeanContext.java
@@ -1,15 +1,15 @@
 package au.com.jcloud.context;
 
-import au.com.jcloud.jcframe.servlet.UserRoleFilter;
+import au.com.jcloud.util.Constants;
 import au.com.jcloud.model.User;
 import net.sourceforge.stripes.action.ActionBeanContext;
 
 public class JCActionBeanContext extends ActionBeanContext {
 	public void setUser(User user) {
-		getRequest().getSession().setAttribute(UserRoleFilter.SESSION_ATTRIBUTE_USER, user);
+		getRequest().getSession().setAttribute(Constants.SESSION_ATTRIBUTE_USER, user);
 	}
 
 	public User getUser() {
-		return (User) getRequest().getSession().getAttribute(UserRoleFilter.SESSION_ATTRIBUTE_USER);
+		return (User) getRequest().getSession().getAttribute(Constants.SESSION_ATTRIBUTE_USER);
 	}
 }
