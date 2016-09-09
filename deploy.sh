ca6380f60cd3858f274e87ea53edca2b6ae97866
David Vittor <davidv@booktopia.com.au>
2016-09-10 08:35:16 +1000

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 
 mvn package
 
+TODAY=`date +%Y-%m-%d`
 TOMCATDIR=/opt/tomcat/tomcat-stage
+#TOMCATDIR=/c/apps/tomcat/apache-tomcat-8.0.35
 
 rm -rf $TOMCATDIR/webapps/website*
 sleep 2
 cp target/website.war $TOMCATDIR/webapps/
-tail -100f $TOMCATDIR/logs/catalina.out 
+tail -100f $TOMCATDIR/logs/catalina.${TODAY}.log 
