e7d79c025fde68198a508f7e72e2e590f2dc0bb3
Digital Spider <dev@digitalspider.com.au>
2016-09-29 08:10:21 +1000

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ mvn package
 TOMCATDIR=/opt/tomcat/tomcat-stage
 #TOMCATDIR=/c/apps/tomcat/apache-tomcat-8.0.35
 
-rm -rf $TOMCATDIR/webapps/website*
+rm -rf $TOMCATDIR/webapps/website.war
 sleep 2
 cp target/website.war $TOMCATDIR/webapps/
 tail -100f $TOMCATDIR/logs/catalina.out | sed -e 's/\(.*WARN.*\)/\o033[33m\1\o033[39m/' -e 's/\(.*ERROR.*\)/\o033[31m\1\o033[39m/'
